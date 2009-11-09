03f656cfd9470b030e172d917e95d35261921edb
Matt Darby <matt@matt-darby.com>
2009-11-08 23:10:53 -0500

    Fixed Chart on the fly resizing

--- public/application.js
+++ public/application.js
@@ -1,15 +1,28 @@
+/*
+
+  TODO:
+  Arrows made by #pointsTo should come off the right end of the box, not the bottom
+  Add day of month Timeline strip
+
+*/
+
+
+
+
+
 // Default class properties
 Box.height           = 12;
-Box.offset           = Box.height * 2;
 Box.radius           = Box.height / 6;
 Box.color            = "#939393";
-Box.area_height      = Box.height + Box.offset;
+Box.area_height      = Box.height * 2;
 
 Timeline.size        = 20;
-Timeline.area_height = Timeline.size * 2;
+Timeline.area_height = Timeline.size * 3;
 Timeline.today_color = "#d1f3ff";
 
 CompletionBox.color  = "#DFDFDF";
+
+
 
 
 
@@ -79,6 +92,151 @@ Date.prototype.equals = function(other){
 
 
 
+
+
+// Arrow class
+function Arrow(start_box, end_box, canvas){
+  this.from   = start_box;
+  this.to     = end_box;
+  this.canvas = canvas;
+
+  if(this.from.start_date < this.to.start_date){
+    new Line(this.from.bottomLeftPoint(), this.cruxPoint(), this.canvas);
+    new Line(this.cruxPoint(), this.to.leftCenterPoint(), this.canvas);
+    this.drawNib("East");
+
+  } else if(this.from.start_date.equals(this.to.start_date)){
+    new Line(this.from.bottomLeftPoint(), this.to.topLeftPoint(), this.canvas);
+    this.drawNib("South");
+  }
+}
+
+Arrow.prototype.cruxPoint = function(){
+  var x = this.from.bottomLeftPoint().x;
+  var y = this.to.leftCenterPoint().y;
+
+  return new Point(x, y);
+}
+
+Arrow.prototype.drawNib = function(direction){
+  if(direction == "East"){
+    var point = this.to.leftCenterPoint();
+    var tip   = point.shift(-2, 0);
+  } else if(direction == "South"){
+    var point = this.to.topLeftPoint();
+    var tip   = point.shift(0, -1);
+  }
+
+  var nib = new Triangle(tip, 3, direction, this.canvas);
+  nib.shape.attr({fill: "#000"}).toBack();
+}
+
+
+
+
+
+// Box class
+function Box(start_date, end_date, chart){
+  this.start_date = start_date;
+  this.end_date   = end_date;
+  this.chart      = chart;
+  this.start_day  = this.chart.timeline.dayAt(start_date);
+  this.end_day    = this.chart.timeline.dayAt(end_date);
+  this.num_days   = this.start_date.daysUntil(this.end_date);
+  this.x          = this.start_day.topLeft().x;
+  this.y          = Timeline.area_height + (Box.area_height * this.chart.numBoxes());
+  this.w          = (this.start_date.daysUntil(this.end_date) * Timeline.size) + Timeline.size;
+  this.h          = Box.height;
+  this.canvas     = this.chart.canvas;
+  this.shape      = this.draw();
+
+  new CompletionBox(this);
+
+  this.chart.appendBox(this);
+}
+
+Box.prototype.draw = function(){
+  var s = this.canvas.rect(this.x, this.y, this.w, this.h, Box.radius);
+  s.attr({fill: Box.color, stroke: "#999"});
+
+  return s;
+}
+
+Box.prototype.topLeftPoint = function(){
+  var x = this.x + (Timeline.size / 2);
+  var y = this.y;
+
+  return new Point(x,y);
+}
+
+Box.prototype.bottomLeftPoint = function(){
+  var x = this.x + (Timeline.size / 2);
+  var y = this.y + this.shape.getBBox().height;
+
+  return new Point(x,y);
+}
+
+Box.prototype.leftCenterPoint = function(){
+  var y = this.y + (this.shape.getBBox().height / 2);
+
+  return new Point(this.x, y);
+}
+
+Box.prototype.rightCenterPoint = function(){
+  var x = this.x + this.shape.getBBox().width;
+  var y = this.leftCenterPoint().y;
+
+  return new Point(x, y);
+}
+
+Box.prototype.pointsTo = function(arr){
+  if(arr.length == undefined){
+    arr = new Array(arr);
+  }
+
+  for(i=0; i < arr.length; i++){
+    new Arrow(this, arr[i], this.canvas);
+  }
+
+  return this;
+}
+
+Box.prototype.says = function(text){
+  var p      = this.rightCenterPoint();
+  var t      = this.canvas.text(p.x, p.y, text);
+  var offset = (t.getBBox().width / 2) + (Box.height / 2);
+
+  t.translate(offset, 0).attr({fill: "#000"}).toFront();
+
+  return this;
+}
+
+Box.prototype.daysCompleted = function(){
+  var curr_date = Date.today();
+
+  if(curr_date < this.start_date){
+    return 0;
+  }
+
+  if(curr_date > this.end_date){
+    return this.num_days;
+  }
+
+  for(i=0; i<this.num_days; i++){
+    var s = this.start_date.clone();
+    var d = s.add(i).days();
+
+    if(d.equals(curr_date)){
+      return i+1;
+    }
+  }
+
+}
+
+
+
+
+
 // Chart class
 function Chart(start_date, end_date){
   this.start_date = start_date;
@@ -105,6 +263,164 @@ Chart.prototype.resize = function(){
   this.canvas.setSize(this.width, this.height);
   this.timeline.drawDayGrid();
 }
+
+
+
+
+
+// CompletionBox class
+function CompletionBox(box){
+  this.box    = box;
+  this.canvas = this.box.canvas;
+
+  this.draw();
+}
+
+CompletionBox.prototype.draw = function(){
+  var num_days = this.box.daysCompleted();
+
+  if(num_days > 0){
+    var x = this.box.x + 1;
+    var y = this.box.y + 1;
+    var h = Box.height - 2;
+    var w = (num_days * Timeline.size) - 2;
+
+    this.shape = this.canvas.rect(x, y, w, h, Box.radius)
+    this.shape.attr({fill: CompletionBox.color, "stroke-width": 0}).toFront();
+  }
+
+  return this;
+}
+
+
+
+
+
+// Day class
+function Day(x, y, date, canvas){
+  this.x        = x;
+  this.y        = y;
+  this.date     = date;
+  this.canvas   = canvas;
+  this.day_abbr = new Array("S","M","T","W","T","F","S");
+  this.label    = this.day_abbr[i%7];
+  this.shape    = null;
+
+  this.draw();
+}
+
+Day.prototype.centerPoint = function(){
+  var x = this.x + (Timeline.size / 2);
+  var y = this.y + (Timeline.size / 2) + 2;
+
+  return new Point(x,y);
+}
+
+Day.prototype.draw = function(){
+  var p = this.centerPoint();
+
+  this.shape = this.canvas.rect(this.x, this.y, Timeline.size, Timeline.size).attr({fill: "#efefef"});
+  this.canvas.text(p.x, p.y, this.label);
+
+
+}
+
+Day.prototype.topLeft = function(){
+  return new Point(this.x, this.y);
+}
+
+
+
+
+
+// GridLine class
+function GridLine(x, y, chart){
+  this.x      = x;
+  this.y      = y;
+  this.chart  = chart;
+  this.canvas = this.chart.canvas;
+
+  this.draw();
+}
+
+GridLine.prototype.draw = function(){
+  var from  = new Point(this.x, this.y);
+  var to    = new Point(this.x, this.y + Box.area_height);
+
+  this.shape = new Line(from, to, this.canvas).shape;
+  this.shape.attr({"stroke-opacity": 0.06}).toBack();
+}
+
+
+
+
+
+// Line class
+function Line(from, to, canvas){
+  this.from   = from;
+  this.to     = to;
+  this.canvas = canvas;
+
+  this.draw();
+}
+
+Line.prototype.draw = function(){
+  var str = "";
+  str     = str.concat("M"+ this.from);
+  str     = str.concat("L"+ this.to);
+
+  this.shape = this.canvas.path(str);
+}
+
+
+
+
+
+// Month class
+function Month(x, y, w, label, canvas){
+  this.x       = x;
+  this.y       = y;
+  this.w       = w;
+  this.label   = label;
+  this.label_x = this.x + (this.w / 2);
+  this.canvas  = canvas;
+
+  this.draw();
+}
+
+Month.prototype.draw = function(){
+  this.canvas.rect(this.x, -1, this.w, Timeline.size + 1).attr({fill: "#BD0000", stroke: "#FFF"});
+  this.canvas.text(this.label_x, this.y / 2, this.label).attr({fill: "#FFF"});
+}
+
+
+
+
+
+// Point class
+function Point(x,y){
+  this.x = parseInt(x);
+  this.y = parseInt(y);
+}
+
+Point.prototype.toString = function(){
+  return this.x +","+ this.y;
+}
+
+Point.prototype.shift = function(x,y){
+  var x = (this.x + x);
+  var y = (this.y + y);
+  return new Point(x,y);
+}
+
+Point.prototype.draw = function(canvas){
+  canvas.text(this.x, (this.y - 10), "("+ this +")").attr({fill: "red"});
+  canvas.circle(this.x, this.y, 3).attr({fill: "red", stroke: "#FFF"});
+
+  return this;
+}
+
+
 
 
 
@@ -186,6 +502,8 @@ Timeline.prototype.drawDayGrid = function(){
 
 
 
+
+
 function TodayBox(x, y, chart){
   this.x      = x;
   this.y      = y;
@@ -201,251 +519,6 @@ TodayBox.prototype.draw = function(){
 }
 
 
-
-// GridLine class
-function GridLine(x, y, chart){
-  this.x      = x;
-  this.y      = y;
-  this.chart  = chart;
-  this.canvas = this.chart.canvas;
-
-  this.draw();
-}
-
-GridLine.prototype.draw = function(){
-  var from  = new Point(this.x, this.y);
-  var to    = new Point(this.x, this.y + Box.area_height);
-
-  this.shape = new Line(from, to, this.canvas).shape;
-  this.shape.attr({"stroke-dasharray": ". ", "stroke-opacity": 0.3}).toBack();
-}
-
-
-
-// Month class
-function Month(x, y, w, label, canvas){
-  this.x       = x;
-  this.y       = y;
-  this.w       = w;
-  this.label   = label;
-  this.label_x = this.x + (this.w / 2);
-  this.canvas  = canvas;
-
-  this.draw();
-}
-
-Month.prototype.draw = function(){
-  this.canvas.rect(this.x, -1, this.w, Timeline.size + 1).attr({fill: "#BD0000", stroke: "#FFF"});
-  this.canvas.text(this.label_x, this.y / 2, this.label).attr({fill: "#FFF"});
-}
-
-
-
-// Day class
-function Day(x, y, date, canvas){
-  this.x        = x;
-  this.y        = y;
-  this.date     = date;
-  this.canvas   = canvas;
-  this.day_abbr = new Array("S","M","T","W","T","F","S");
-  this.label    = this.day_abbr[i%7];
-  this.shape    = null;
-
-  this.draw();
-}
-
-Day.prototype.centerPoint = function(){
-  var x = this.x + (Timeline.size / 2);
-  var y = this.y + (Timeline.size / 2) + 2;
-
-  return new Point(x,y);
-}
-
-Day.prototype.draw = function(){
-  var p = this.centerPoint();
-
-  this.shape = this.canvas.rect(this.x, this.y, Timeline.size, Timeline.size).attr({fill: "#efefef"});
-  this.canvas.text(p.x, p.y, this.label);
-
-
-}
-
-Day.prototype.topLeft = function(){
-  return new Point(this.x, this.y);
-}
-
-
-
-// Point class
-function Point(x,y){
-  this.x = parseInt(x);
-  this.y = parseInt(y);
-}
-
-Point.prototype.toString = function(){
-  return this.x +","+ this.y;
-}
-
-Point.prototype.shift = function(x,y){
-  var x = (this.x + x);
-  var y = (this.y + y);
-  return new Point(x,y);
-}
-
-Point.prototype.draw = function(canvas){
-  canvas.text(this.x, (this.y - 10), "("+ this +")").attr({fill: "red"});
-  canvas.circle(this.x, this.y, 3).attr({fill: "red", stroke: "#FFF"});
-
-  return this;
-}
-
-
-
-// Box class
-function Box(start_date, end_date, chart){
-  this.start_date = start_date;
-  this.end_date   = end_date;
-  this.chart      = chart;
-  this.start_day  = this.chart.timeline.dayAt(start_date);
-  this.end_day    = this.chart.timeline.dayAt(end_date);
-  this.num_days   = this.start_date.daysUntil(this.end_date);
-  this.x          = this.start_day.topLeft().x;
-  this.y          = (Timeline.size * 3) + (Box.offset * this.chart.numBoxes());
-  this.w          = (this.start_date.daysUntil(this.end_date) * Timeline.size) + Timeline.size;
-  this.h          = Box.height;
-  this.canvas     = this.chart.canvas;
-  this.shape      = this.draw();
-
-  new CompletionBox(this);
-
-  this.chart.appendBox(this);
-}
-
-Box.prototype.draw = function(){
-  var s = this.canvas.rect(this.x, this.y, this.w, this.h, Box.radius);
-  s.attr({fill: Box.color, stroke: "#999"});
-
-  return s;
-}
-
-Box.prototype.bottomLeftPoint = function(){
-  var x = this.x + (Timeline.size / 2);
-  var y = this.y + this.shape.getBBox().height;
-
-  return new Point(x,y);
-}
-
-Box.prototype.leftCenterPoint = function(){
-  var y = this.y + (this.shape.getBBox().height / 2);
-
-  return new Point(this.x, y);
-}
-
-Box.prototype.rightCenterPoint = function(){
-  var x = this.x + this.shape.getBBox().width;
-  var y = this.leftCenterPoint().y;
-
-  return new Point(x, y);
-}
-
-Box.prototype.pointsTo = function(arr){
-  if(arr.length == undefined){
-    arr = new Array(arr);
-  }
-
-  for(i=0; i < arr.length; i++){
-    new Arrow(this, arr[i], this.canvas);
-  }
-
-  return this;
-}
-
-Box.prototype.says = function(text){
-  var p      = this.rightCenterPoint();
-  var t      = this.canvas.text(p.x, p.y, text);
-  var offset = (t.getBBox().width / 2) + (Box.height / 2);
-
-  t.translate(offset, 0).attr({fill: "#000"}).toFront();
-
-  return this;
-}
-
-Box.prototype.daysCompleted = function(){
-  var curr_date = Date.today();
-
-  if(curr_date < this.start_date){
-    return 0;
-  }
-
-  if(curr_date > this.end_date){
-    return this.num_days;
-  }
-
-  for(i=0; i<this.num_days; i++){
-    var s = this.start_date.clone();
-    var d = s.add(i).days();
-
-    if(d.equals(curr_date)){
-      return i+1;
-    }
-  }
-
-}
-
-
-
-// CompletionBox class
-function CompletionBox(box){
-  this.box    = box;
-  this.canvas = this.box.canvas;
-
-  this.draw();
-}
-
-CompletionBox.prototype.draw = function(){
-  var num_days = this.box.daysCompleted();
-
-  if(num_days > 0){
-    var x = this.box.x + 1;
-    var y = this.box.y + 1;
-    var h = Box.height - 2;
-    var w = (num_days * Timeline.size) - 2;
-
-    this.shape = this.canvas.rect(x, y, w, h, Box.radius)
-    this.shape.attr({fill: CompletionBox.color, "stroke-width": 0}).toFront();
-  }
-
-  return this;
-}
-
-
-
-// Arrow class
-function Arrow(start_box, end_box, canvas){
-  this.from   = start_box;
-  this.to     = end_box;
-  this.canvas = canvas;
-
-  new Line(this.from.bottomLeftPoint(), this.cruxPoint(), this.canvas);
-  new Line(this.cruxPoint(), this.to.leftCenterPoint(), this.canvas);
-
-  this.drawNib();
-}
-
-Arrow.prototype.cruxPoint = function(){
-  var x = this.from.bottomLeftPoint().x;
-  var y = this.to.leftCenterPoint().y;
-
-  return new Point(x, y);
-}
-
-Arrow.prototype.drawNib = function(){
-  var point  = this.to.leftCenterPoint();
-  var tip    = point.shift(-2, 0);
-
-  var nib = new Triangle(tip, 3, "East", this.canvas);
-  nib.shape.attr({fill: "#000"}).toBack();
-}
 
 
 
@@ -492,23 +565,6 @@ Triangle.prototype.draw = function(){
 
 
 
-// Line class
-function Line(from, to, canvas){
-  this.from   = from;
-  this.to     = to;
-  this.canvas = canvas;
-
-  this.draw();
-}
-
-Line.prototype.draw = function(){
-  var str = "";
-  str     = str.concat("M"+ this.from);
-  str     = str.concat("L"+ this.to);
-
-  this.shape = this.canvas.path(str);
-}
-
 
 
 // Helper function
@@ -522,6 +578,8 @@ function interrogate(obj){
 
 
 
+
+
 $(document).ready(function(){
   var box1       = new Array(Date.parse("Nov 3, 2009"), Date.parse("Nov 12, 2009"));
   var box2       = new Array(Date.parse("Nov 5, 2009"), Date.parse("Nov 17, 2009"));
@@ -530,6 +588,8 @@ $(document).ready(function(){
   var box5       = new Array(Date.parse("Nov 17, 2009"), Date.parse("Dec 5, 2009"));
   var box6       = new Array(Date.parse("Dec 1, 2009"), Date.parse("Dec 14, 2009"));
   var box7       = new Array(Date.parse("Dec 3, 2009"), Date.parse("Dec 9, 2009"));
+  var box8       = new Array(Date.parse("Dec 3, 2009"), Date.parse("Dec 12, 2009"));
+  var box9       = new Array(Date.parse("Dec 4, 2009"), Date.parse("Dec 15, 2009"));
 
   var start_date = Date.parse("Nov 1, 2009");
   var end_date   = Date.parse("Dec 31, 2009");
@@ -542,6 +602,8 @@ $(document).ready(function(){
   var b5 = new Box(box5[0], box5[1], chart).says("Long!");
   var b6 = new Box(box6[0], box6[1], chart).says("Oh yes!");
   var b7 = new Box(box7[0], box7[1], chart).says("Something");
+  var b8 = new Box(box8[0], box8[1], chart).says("Same Day");
+  var b9 = new Box(box9[0], box9[1], chart).says("Next Day");
 
   b1.pointsTo(b2);
   b2.pointsTo(b3);
@@ -549,4 +611,6 @@ $(document).ready(function(){
   b3.pointsTo(b5);
   b5.pointsTo(b6);
   b6.pointsTo(b7);
+  b7.pointsTo(b8);
+  b8.pointsTo(b9);
 });
