d47e16510d9816829e800eb513d55e9bbeac7cbf
Matt Darby <matt@matt-darby.com>
2009-11-10 12:18:26 -0500

    Updated /json to current spec

--- server.rb
+++ server.rb
@@ -11,17 +11,26 @@ end
 get '/json' do
   content_type :json
 
-  [
-    {:start_date => "Nov 3, 2009", :end_date => "Nov 12, 2009", :task => "ASDF", :resource => "MD"},
-    {:start_date => "Nov 5, 2009", :end_date => "Nov 17, 2009", :task => "Working?", :resource => "GM"},
-    {:start_date => "Nov 15, 2009", :end_date => "Nov 23, 2009", :task => "Whoa...", :resource => "TM"},
-    {:start_date => "Nov 17, 2009", :end_date => "Nov 25, 2009", :task => "Hrm", :resource => "GG"},
-    {:start_date => "Nov 17, 2009", :end_date => "Dec 5, 2009", :task => "Long!", :resource => "KJ"},
-    {:start_date => "Dec 1, 2009", :end_date => "Dec 14, 2009", :task => "Oh yes!", :resource => "KH"},
-    {:start_date => "Dec 3, 2009", :end_date => "Dec 9, 2009", :task => "Something", :resource => "LS"},
-    {:start_date => "Dec 3, 2009", :end_date => "Dec 12, 2009", :task => "Same Day", :resource => "SS"},
-    {:start_date => "Dec 4, 2009", :end_date => "Dec 15, 2009", :task => "Next Day", :resource => "GD"}
-  ].to_json
+  {
+    :boxes => [
+      {:start_date => "Nov 3, 2009", :end_date => "Nov 12, 2009", :label => "ASDF", :resource => "MD"},
+      {:start_date => "Nov 5, 2009", :end_date => "Nov 17, 2009", :label => "Working?", :resource => "GM"},
+      {:start_date => "Nov 15, 2009", :end_date => "Nov 23, 2009", :label => "Whoa...", :resource => "TM"},
+      {:start_date => "Nov 17, 2009", :end_date => "Nov 25, 2009", :label => "Hrm", :resource => "GG"},
+      {:start_date => "Nov 17, 2009", :end_date => "Dec 5, 2009", :label => "Long!", :resource => "KJ"},
+      {:start_date => "Dec 1, 2009", :end_date => "Dec 14, 2009", :label => "Oh yes!", :resource => "KH"},
+      {:start_date => "Dec 3, 2009", :end_date => "Dec 9, 2009", :label => "Something", :resource => "LS"},
+      {:start_date => "Dec 3, 2009", :end_date => "Dec 12, 2009", :label => "Same Day", :resource => "SS"},
+      {:start_date => "Dec 4, 2009", :end_date => "Dec 15, 2009", :label => "Next Day", :resource => "GD"},
+      {:start_date => "Dec 7, 2009", :end_date => "Dec 9, 2009", :label => "Smaller", :resource => "MQ"}
+    ],
+    :arrows => [
+      {:from => 0, :to => [1,2]},
+      {:from => 1, :to => 2},
+      {:from => 3, :to => [5,6]},
+      {:from => 6, :to => 7}
+    ]
+  }.to_json
 end
 
 get '/stylesheet.css' do
